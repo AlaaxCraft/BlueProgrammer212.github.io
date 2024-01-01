61adb4513ae551bf2c77a99fbc4cfc3af25770ad
BlueProgrammer212 <87359245+BlueProgrammer212@users.noreply.github.com>
2024-01-01 14:38:15 +0800

    Improved the content "a bit"

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-// Why r u even reading this code?
-const PP = document.getElementById("ppSize");
-let ppSize = 0;
-let x = 0;
+const ageSpan = document.getElementById("age");
+const chessEloSpan = document.getElementById("chess-elo");
 
-setInterval(() => {
-    x += 0.1;
-    ppSize = Math.round(Math.abs(Math.cos(x) * 10));
-    PP.textContent = `8${Array(ppSize).fill("=").join("")}D`
-}, 100)
+const dateNow = new Date();
+const birthDate = new Date("2008-08-08 9:00:00");
+
+/**
+ * 
+ * @param {Date} date1 
+ * @param {Date} date2 
+ * @returns the year difference between two dates.
+ */
+function getYearDistance(date1, date2) {
+    let diff = date2.getFullYear() - date1.getFullYear();
+
+    if (date2.getMonth() < date1.getMonth() ||
+        (date2.getMonth() == date1.getMonth() && date2.getDate() < date1.getDate())) {
+        diff--;
+    }
+
+    return diff;
+}
+
+function getCurrentChessElo(username) {
+    fetch(`https://api.chess.com/pub/player/${username}/stats`).then(data => data.json())
+        .then(response => {
+            chessEloSpan.textContent = response.chess_rapid.last.rating;
+        })
+}
+
+ageSpan.textContent = getYearDistance(birthDate, dateNow);
+chessEloSpan.textContent = getCurrentChessElo("blueprogrammer212");
